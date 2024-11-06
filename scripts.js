c32b5c5175053d98237724c7182d00c735f878ec
Arvid Edvinsson <ae225aw@student.lnu.se>
2024-11-06 17:42:09 +0900

    create 16x16 grid of divs

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 const box = document.querySelector('#box')
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const divs = document.createElement('div')
+        box.appendChild(divs)
+    }
+}
